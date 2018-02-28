991486b68c0483910f0094333e8b75fc9aa99966
William McLendon <wcmclen@sandia.gov>
2018-02-28 12:31:07 -0700

    updated

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+  echo "I AM A PULL REQUEST!"
+else
+  echo "I AM NOT A PULL REQUEST!"
+fi
+
 if [ ! -d "Scripts" ]; then
   mkdir Scripts
 fi
